cbf0022f4ead022a3f6d03c9205a01d2256e7055
mayankpal20 <mayankpal5820@gmail.com>
2024-05-21 04:18:10 +0530

    Action Methods done

---
+++ Lecture no 13 ( Action Methods )/ActionMethods/ActionMethods/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string NewMethod()
+        {
+            return "AA gaya Swad";
+        }
+    }
+}
--- Lecture no 13 ( Action Methods )/ActionMethods/ActionMethods/obj/Debug/net8.0/ActionMethods.AssemblyInfo.cs
+++ Lecture no 13 ( Action Methods )/ActionMethods/ActionMethods/obj/Debug/net8.0/ActionMethods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActionMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3a1f06256bd0a202550668729d645352d4a3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActionMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActionMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
