eea2c899007e47c29c1c8bdda1aaa90a8ea65349
mayankpal20 <mayankpal5820@gmail.com>
2024-06-05 11:40:47 +0530

    Lecture 18

---
+++ Lecture no 18 ( ViewBag )/Lecture no 18 ( ViewBag )/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Lecture_no_18___ViewBag__.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lecture_no_18___ViewBag__.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.data1 = "mojle";
+            ViewBag.data2 = 902;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lecture no 18 ( ViewBag )/Lecture no 18 ( ViewBag )/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lecture_no_18___ViewBag__.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
